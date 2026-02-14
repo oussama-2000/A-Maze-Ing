28e2be17ed3e2d1c351ac51481d5858976f1a624
Oussama Taoussi <outaouss@e1r5p14.1337.ma>
2026-02-14 17:51:39 +0100

    msg_directed

--- mazegen/cell.py
+++ mazegen/cell.py
@@ -6,6 +6,8 @@ class Cell:
             visited = if the cell was visited or not
             blocked if the cell is owned by 42 block
     """
+    flag = True
+
     def __init__(self) -> None:
         self.walls = {
             "N": True,
